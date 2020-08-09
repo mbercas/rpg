faebda4d17e7a75cd86263c51f190e236774b4d0
Manuel Berrocal <manuel@manuellaptop2.ezono.net>
2020-08-09 16:55:10 +0200

    Episode 1
     - remove not needed files from project skeleton
     - rename executable as game
     - create window with check-box for plan

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
-#include <functional>
 #include <iostream>
 
 #include <spdlog/spdlog.h>
-
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
 #include <docopt/docopt.h>
 
-#include <iostream>
 
 static constexpr auto USAGE =
-  R"(Naval Fate.
+    R"(Naval Fate.
 
     Usage:
           naval_fate ship new <name>...
@@ -26,20 +28,85 @@ static constexpr auto USAGE =
           --drifting    Drifting mine.
 )";
 
-int main(int argc, const char **argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]]const char **argv)
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+    // std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
+    //                                                            { std::next(argv), std::next(argv, argc) },
+    //                                                            true,// show help if requested
+    //                                                            "Naval Fate 2.0");// version string
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
-  }
+    // for (auto const &arg : args) {
+    //     std::cout << arg.first << arg.second << std::endl;
+    // }
 
 
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
+    //Use the default logger (stdout, multi-threaded, colored)
+    spdlog::info("Starting ImGUI + SFML");
 
-  fmt::print("Hello, from {}\n", "{fmt}");
+    fmt::print("Hello, from {}\n", "{fmt}");
+
+    sf::RenderWindow window(sf::VideoMode(1024, 760), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    constexpr double scale_factor = 1.2f;
+    ImGui::GetStyle().ScaleAllSizes(scale_factor);
+    ImGui::GetIO().FontGlobalScale = scale_factor;
+
+
+    // game status - keep outside of the main loop
+    bool state0(false);
+    bool state1(false);
+    bool state2(false);
+    bool state3(false);
+    bool state4(false);
+    bool state5(false);
+    bool state6(false);
+    bool state7(false);
+    bool state8(false);
+    bool state9(false);
+    bool state10(false);
+    
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+//    ImGui::ShowDemoWindow();
+
+        ImGui::Begin("The Plan");
+        ImGui::Button("Look at this pretty button");
+
+
+        ImGui::Checkbox(" 0: The Plan", &state0);
+     
+        ImGui::Checkbox(" 1: Getting Started", &state1);
+        ImGui::Checkbox(" 2: C++ 20 So Far", &state2);
+        ImGui::Checkbox(" 3: Reading SFML Input States", &state3);
+        ImGui::Checkbox(" 4: Managing Game State", &state4);
+        ImGui::Checkbox(" 5: Making Our Game Testable", &state5);
+        ImGui::Checkbox(" 6: Making Game State Allocator Aware", &state6);
+        ImGui::Checkbox(" 7: Add logging To Game Engine", &state7);
+        ImGui::Checkbox(" 8: Draw A Game Map", &state8);
+        ImGui::Checkbox(" 9: Dialog Trees", &state9);
+        ImGui::Checkbox("10: Porting From SFML to SDL", &state10);
+
+        ImGui::End();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
 }
